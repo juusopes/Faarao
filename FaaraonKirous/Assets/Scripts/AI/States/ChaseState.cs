d0734d0e3004b4b3ede18ed25ccd056f5d4d3329
HenriLagerroos <43806027+HenriLagerroos@users.noreply.github.com>
2020-10-02 11:17:25 +0300

    init
    init

---
+++ FaaraonKirous/Assets/Scripts/AI/States/ChaseState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ChaseState : State
+{
+    public ChaseState(Character character, StateMachine stateMachine) : base(character, stateMachine) { }
+
+    public override void Tick()
+    {
+        Chase();
+        Look();
+    }
+
+    private void Chase()
+    {
+        character.SetDestination(character.chaseTarget);
+    }
+
+    void Look()
+    {
+        if (!character.CanChasePlayer())
+        {
+            character.LostTrackOfPlayer();
+            ToTrackingState();
+        }
+    }
+}
+
