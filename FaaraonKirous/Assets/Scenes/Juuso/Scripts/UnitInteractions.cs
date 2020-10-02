923ee72797623217519f71e9eaa6f2ef06b779de
juusopes <36258359+juusopes@users.noreply.github.com>
2020-10-02 11:56:38 +0300

    ..
    ..

---
+++ FaaraonKirous/Assets/Scenes/Juuso/Scripts/UnitInteractions.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitInteractions : MonoBehaviour
+{
+    public float cooldownTime;
+    public float nextFireTime;
+    public bool isStanding = true;
+    public bool onCooldown = false;
+
+    public Texture2D cursor, cursorInteract, cursorAttack;
+    public bool checkbox;
+    public GameObject standing, crouching;
+    public Image imageCooldown;
+
+    private void Start()
+    {
+        crouching.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        bool menuActivated = GetComponent<InGameMenu>().menuActive;
+
+        // cannot use abilities etc. if menu is active
+        if (menuActivated == false)
+        {
+            if (Input.GetButtonDown("Stance"))
+            {
+                if (isStanding)
+                {
+                    print("Unit is now crouching");
+                    standing.SetActive(false);
+                    crouching.SetActive(true);
+                    isStanding = false;
+                }
+                else
+                {
+                    print("Unit is now standing");
+                    standing.SetActive(true);
+                    crouching.SetActive(false);
+                    isStanding = true;
+                }
+            }
+
+            if (Input.GetButton("Attack"))
+            {
+                Cursor.SetCursor(cursorAttack, Vector2.zero, CursorMode.ForceSoftware);
+            }
+            else if (Input.GetButton("Interact"))
+            {
+                Cursor.SetCursor(cursorInteract, Vector2.zero, CursorMode.ForceSoftware);
+            }
+            else
+            {
+                Cursor.SetCursor(cursor, Vector2.zero, CursorMode.ForceSoftware);
+            }
+
+            if (Time.time > nextFireTime)
+            {
+                onCooldown = false;
+                imageCooldown.fillAmount = 1;
+
+                if (Input.GetButtonDown("Ability"))
+                {
+                    cooldownTime = 5;
+                    onCooldown = true;
+
+                    nextFireTime = Time.time + cooldownTime;
+                }
+            }
+
+            if (onCooldown)
+            {
+                imageCooldown.fillAmount -= 1 / cooldownTime * Time.deltaTime;
+            }
+            if (!onCooldown)
+            {
+                imageCooldown.fillAmount = 0;
+            }
+        }
+    }
+}
---
+++ FaaraonKirous/Assets/Scripts/AI/Combat/FieldOfView.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        float fov = 90f;
+        Vector3 origin = Vector3.zero;
+        int rayCount = 50;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 50f;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for(int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, UtilsClass.GetVectorFromAngle(angle), viewDistance);
+            if(raycastHit2D.collider == null)
+            {
+                vertex = origin + UtilsClass.GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+                vertex = raycastHit2D.point;
+            }
+
+
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
