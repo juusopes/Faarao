6f619316ad910a2e18fb128611fea7a861e394b7
juusopes <36258359+juusopes@users.noreply.github.com>
2020-10-07 14:36:40 +0300

    ..
    ..

---
+++ FaaraonKirous/Assets/Scripts/AI/StateMachine.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    private State currentState;
+
+    [HideInInspector]
+    public PatrolState patrolState;
+    [HideInInspector]
+    public AlertState alertState;
+    [HideInInspector]
+    public ChaseState chaseState;
+    [HideInInspector]
+    public TrackingState trackingState;
+    Character character;
+    public StateMachine(Character owner)
+    {
+        character = owner;
+        patrolState = new PatrolState(owner, this);
+        alertState = new AlertState(owner, this);
+        chaseState = new ChaseState(owner, this);
+        trackingState = new TrackingState(owner, this);
+
+        SetState(patrolState);
+    }
+
+    public void UpdateSM()
+    {
+        currentState.Tick();
+    }
+
+    public void SetState(State state)
+    {
+        if (currentState != null)
+            currentState.OnStateExit();
+
+        currentState = state;
+        character.gameObject.name = "Enemy State - " + state.GetType().Name;
+
+        if (currentState != null)
+            currentState.OnStateEnter();
+
+        SetIndicator();
+    }
+
+    public void SetIndicator()
+    {
+        if (currentState == patrolState)
+            character.UpdateIndicator(Color.green);
+        else if (currentState == chaseState)
+            character.UpdateIndicator(Color.red);
+        else if (currentState == alertState)
+            character.UpdateIndicator(Color.yellow);
+        else if (currentState == trackingState)
+            character.UpdateIndicator(Color.blue);
+    }
+
+
+
+}
---
+++ FaaraonKirous/Assets/Scripts/AI/Utilities/UtilsClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilsClass 
+{ 
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        //angle = 0 -> 360
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+    public static bool Approximately(this Quaternion quatA, Quaternion value, float acceptableRange)
+    {
+        return 1 - Mathf.Abs(Quaternion.Dot(quatA, value)) < acceptableRange;
+    }
+
+    //Todo: test this ..
+    public static Quaternion Closer(this Quaternion orig, Quaternion quatA, Quaternion quatB)
+    {
+        float a = Quaternion.Dot(orig, quatA);
+        float b = Quaternion.Dot(orig, quatB);
+        return a > b ? quatA : quatB;
+    }
+}
