7eb9e30a75586c36c3c6c6580e220792d126f1c0
juusopes <36258359+juusopes@users.noreply.github.com>
2020-10-07 14:18:39 +0300

    ..
    ..

---
+++ FaaraonKirous2/Assets/Scripts/AI/States/AlertState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class AlertState : State
+{
+    private float searchTimer;
+
+    public AlertState(Character character, StateMachine stateMachine) : base(character, stateMachine){}
+
+    public override void Tick()
+    {
+        Search();
+        Look();
+    }
+
+ 
+    void Look()
+    {
+        if(character.CanSeePlayer())
+            ToChaseState();
+    }
+
+
+    void Search()
+    {
+        character.StopDestination();
+        character.SearchRotate();
+        searchTimer += Time.deltaTime;
+        if (searchTimer >= character.classSettings.searchingDuration)
+        {
+            stateMachine.SetState(stateMachine.patrolState);
+            ToPatrolState();
+        }
+
+    }
+
+}
+
