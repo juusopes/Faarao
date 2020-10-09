ab088bab147c6b9d64a025b8a6a34b1378f22668
HenriLagerroos <43806027+HenriLagerroos@users.noreply.github.com>
2020-10-09 15:27:33 +0300

    Linerender experiment

---
+++ FaaraonKirous/Assets/Scripts/AI/LineMaterials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/LineColor", order = 1)]
+public class LineMaterials : ScriptableObject
+{
+    public Material RedLine;
+    public Material YellowLine;
+    public Material GreenLine;
+}
+
