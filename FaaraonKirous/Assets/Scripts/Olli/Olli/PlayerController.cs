97b834519b4787647c9b96dc3711b1558f484d70
Olmavira <Olmavira@users.noreply.github.com>
2020-10-20 10:22:13 +0300

    Fixes

---
+++ FaaraonKirous/Assets/Scripts/Olli/Olli/PlayerController.cs
@@ -0,0 +1,342 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    //Which Player
+    public bool playerOne;
+    //Moving
+    public float movementSpeed;
+    public float doubleClickTimer;
+    public bool isRunning;
+    public bool isActiveCharacter;
+    private NavMeshAgent navMeshAgent;
+    private Vector3 targetV3;
+    private Vector3 position;
+
+    //PlayerSynergy
+    private GameObject anotherCharacter;
+    private bool lineOfSight;
+
+    //Crouch
+    public bool isCrouching;
+
+    //Abilities
+    //Indicator
+    [HideInInspector]
+    public bool abilityIsActive;
+    public GameObject indicator;
+    private GameObject line;
+    [HideInInspector]
+    public GameObject visibleInd;
+
+    //Invisibility
+    public bool isInvisible;
+
+    //Camera
+    private GameObject camControl;
+
+    //Interactive
+    public GameObject interactObject;
+
+    //Climbing
+    public GameObject climbObject;
+    private GameObject savedClimbable;
+    private bool climbing;
+    private bool climbSuccess;
+    public bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moving();
+        LineOfSight();
+        KeyControls();
+        Invisibility();
+        SetIndicator();
+        Climb();
+    }
+
+    private void Initialize()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        targetV3 = transform.position;
+        camControl = GameObject.FindGameObjectWithTag("MainCamera").transform.parent.gameObject;
+
+        GameObject[] tempCharacters = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject tempCharacter in tempCharacters)
+        {
+            if (tempCharacter != this.gameObject)
+            {
+                anotherCharacter = tempCharacter;
+            }
+        }
+        climbing = false;
+    }
+    public void Moving()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+        //DoubleClick Check
+        if (isActiveCharacter)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (Physics.Raycast(ray, out hit))
+                {
+                    targetV3 = hit.point;
+                }
+                if (isRunning)
+                {
+                    isRunning = false;
+                }
+                if (doubleClickTimer < 0.5f)
+                {
+                    isRunning = true;
+                }
+                if (doubleClickTimer >= 0.5f)
+                {
+                    doubleClickTimer = 0;
+                }
+            }
+        }
+        if (doubleClickTimer < 0.5f)
+        {
+            doubleClickTimer += Time.deltaTime;
+        }
+        //Moving
+        if ((!playerOne && !GetComponent<PriestAbilities>().useTeleknesis) || playerOne)
+        {
+            if (isRunning)
+            {
+                navMeshAgent.speed = movementSpeed * 5f;
+            }
+            else if (isCrouching)
+            {
+                navMeshAgent.speed = movementSpeed * 0.2f;
+            }
+            else
+            {
+                navMeshAgent.speed = movementSpeed;
+            }
+            if (!climbing)
+            {
+                navMeshAgent.SetDestination(targetV3);
+            }
+            if (position == transform.position)
+            {
+                isRunning = false;
+            }
+            else
+            {
+                position = transform.position;
+            }
+        }
+    }
+    private void LineOfSight()
+    {
+        RaycastHit hit;
+        Vector3 fromPosition = transform.position;
+        Vector3 toPosition = anotherCharacter.transform.position;
+        Vector3 direction = toPosition - fromPosition;
+
+        Debug.DrawRay(fromPosition, direction);
+
+        if (Physics.Raycast(transform.position, direction, out hit))
+        {
+            if (hit.collider.tag != "Player")
+            {
+                lineOfSight = false;
+            }
+        }
+        else
+        {
+            lineOfSight = true;
+        }
+    }
+    public void Crouch()
+    {
+        if (isActiveCharacter)
+        {
+            if (!isCrouching)
+            {
+                isCrouching = true;
+            }
+            else
+            {
+                isCrouching = false;
+            }
+        }
+    }
+    private void InvisiblitySpell()
+    {
+        if (isActiveCharacter)
+        {
+            if (lineOfSight)
+            {
+                anotherCharacter.GetComponent<PlayerController>().isInvisible = true;
+            }
+        }
+    }
+
+    private void Invisibility()
+    {
+        if (isInvisible)
+        {
+            this.gameObject.tag = "PlayerInvisible";
+        }
+        else
+        {
+            this.gameObject.tag = "Player";
+        }
+    }
+
+    private void SetIndicator()
+    {
+        if (isActiveCharacter)
+        {
+            if (abilityIsActive)
+            {
+                if (visibleInd == null)
+                {
+                    visibleInd = Instantiate(indicator);
+                    line = visibleInd.transform.GetChild(0).gameObject;
+                    visibleInd.GetComponent<AbilityIndicator>().player = this.gameObject;
+                }
+                else
+                {
+                    visibleInd.SetActive(true);
+                    line.SetActive(true);
+                }
+            }
+            else
+            {
+                if (visibleInd != null)
+                {
+                    visibleInd.SetActive(false);
+                    line.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            if (visibleInd != null)
+            {
+                visibleInd.SetActive(false);
+                line.SetActive(false);
+            }
+        }
+    }
+
+    private void CamFollow()
+    {
+        if (isActiveCharacter)
+        {
+            if (camControl.GetComponent<CameraControl>().camFollow)
+            {
+                camControl.GetComponent<CameraControl>().camFollow = false;
+                camControl.transform.parent = null;
+            }
+            else
+            {
+                camControl.GetComponent<CameraControl>().camFollow = true;
+                camControl.transform.parent = this.gameObject.transform;
+            }
+        }
+    }
+
+    private void Interact()
+    {
+        if (interactObject != null)
+        {
+            if (!interactObject.GetComponent<Activator>().activated)
+            {
+                interactObject.GetComponent<Activator>().activated = true;
+            }
+            else
+            {
+                interactObject.GetComponent<Activator>().activated = false;
+            }
+        }
+    }
+    public void Climb()
+    {
+        if (climbObject != null && climbing)
+        {
+            Vector3 rot = Vector3.RotateTowards(transform.position, climbObject.transform.position, 360, 360);
+            transform.rotation = Quaternion.LookRotation(rot);
+            Debug.Log(climbing);
+            climbSuccess = true;
+            float yAxisValue = 0.01f;
+            GetComponent<Rigidbody>().isKinematic = true;
+            gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.Translate(new Vector3(0, yAxisValue, 0));
+            savedClimbable = climbObject;
+        } else if(climbSuccess)
+        {
+            transform.Translate(new Vector3(0, 0, 0.01f));
+            if (grounded)
+            {
+                climbSuccess = false;
+            }
+        } else
+        {
+            GetComponent<NavMeshAgent>().enabled = true;
+            gameObject.GetComponent<NavMeshAgent>().isStopped = false;
+        }
+    }
+
+    private void KeyControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Crouch();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            InvisiblitySpell();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!abilityIsActive)
+            {
+                abilityIsActive = true;
+            }
+            else
+            {
+                abilityIsActive = false;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            CamFollow();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (climbing)
+            {
+                climbing = false;
+            }
+            else
+            {
+                climbing = true;
+            }
+        }
+    }
+}
+
