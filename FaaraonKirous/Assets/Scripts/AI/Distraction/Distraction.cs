44983aa8038b7ad274e8afdf880966ee691de69f
juusopes <36258359+juusopes@users.noreply.github.com>
2020-10-22 14:51:39 +0300

    revert henkan skene mergeen

---
+++ FaaraonKirous/Assets/Scripts/AI/Distraction/Distraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Distraction : MonoBehaviour
+{
+    [SerializeField]
+    private DistractionClass distractionClass = null;
+    [HideInInspector]
+    public DistractionOption option;
+
+    //Nicer API, nothing else
+    public DetectionType detectionType => distractionClass.detectionType;
+    public DistractionType distractionType => distractionClass.distractionType;
+    public float effectTime => distractionClass.effectTime;
+
+    void Start()
+    {
+        Assert.IsNotNull(distractionClass, "No distraction class set");
+
+        Destroy(this.gameObject, distractionClass.destroyTime);
+    }
+}
