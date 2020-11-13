a4a4d83ca4d2f24dd36ce211137990e7c02ef601
juusopes <36258359+juusopes@users.noreply.github.com>
2020-11-13 10:56:37 +0200

    ovi siirretty

--- FaaraonKirous/Assets/Scripts/OllinScriptit/CameraControl.cs
+++ FaaraonKirous/Assets/Scripts/OllinScriptit/CameraControl.cs
@@ -29,6 +29,8 @@ public class CameraControl : MonoBehaviour
 
     public float rotation;
 
+    public int zPanlimit, xPanlimit;
+
     // Start is called before the first frame update
     void Start()
     {
