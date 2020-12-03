334073b754638e1b430267dcfd7ebd4e12e6f8fc
Joona Eskonpekka <ejaa7991@gmail.com>
2020-12-03 16:32:36 +0200

    oooo

--- FaaraonKirous/Assets/Scripts/UI/LoadUIManager.cs
+++ FaaraonKirous/Assets/Scripts/UI/LoadUIManager.cs
@@ -10,6 +10,12 @@ public class LoadUIManager : MonoBehaviour
     [SerializeField]
     private GameObject _loadMenu;
 
+    [SerializeField]
+    private MenuListCreator _saveMenuListMainMenu;
+
+    [SerializeField]
+    private GameObject _loadMenuMainMenu;
+
     private void Awake()
     {
         if (Instance == null)
@@ -33,11 +39,6 @@ public class LoadUIManager : MonoBehaviour
         return false;
     }
 
-    public void Start()
-    {
-        UpdateSaveList();
-    }
-
     public void UpdateSaveList()
     {
         SaveUIObject[] saveList = GameManager._instance.GetSaveFiles().ToArray();
@@ -45,11 +46,24 @@ public class LoadUIManager : MonoBehaviour
         _saveMenuList.RefreshList(saveList);
     }
 
+    public void UpdateSaveListMainMenu()
+    {
+        SaveUIObject[] saveList = GameManager._instance.GetSaveFiles().ToArray();
+
+        _saveMenuListMainMenu.RefreshList(saveList);
+    }
+
     public void OpenInGameMenu()
     {
         InGameMenu._instance.DeactivateMenu();
         _loadMenu.SetActive(true);
         UpdateSaveList();
+    }
+
+    public void Close()
+    {
+        _loadMenu.SetActive(false);
+        _loadMenuMainMenu.SetActive(false);
     }
 
     public void CloseInGameMenu()
@@ -60,11 +74,15 @@ public class LoadUIManager : MonoBehaviour
 
     public void CloseMainMenu()
     {
+        _loadMenuMainMenu.SetActive(false);
+        MainMenuUIManager.Instance.Open();
         // TODO: Not implemented
     }
 
     public void OpenMainMenu()
     {
+        _loadMenuMainMenu.SetActive(true);
+        UpdateSaveListMainMenu();
         // TODO: Not implemented
     }
 }
